d84ccc51563cb60a695e018ac2fa151b45a4e2a1
Alifioditya <alifioditya@gmail.com>
2023-11-29 13:48:06 +0700

    feat: added class weight hyperparameter to KNN and class documentation

--- src/Models/neighbors.py
+++ src/Models/neighbors.py
@@ -11,8 +11,74 @@ class KNN:
     KNN is a classification algorithm that uses the k-nearest neighbors algorithm.
     It is a lazy learning algorithm that stores all instances corresponding to training data in n-dimensional space.
     When an unknown discrete data is received, it analyzes the closest k number of instances saved (nearest neighbors) and returns the most common class as the prediction and for real-valued data it returns the mean of k nearest neighbors.
+
+    Parameters
+    ----------
+    k : int, default=5
+        Number of neighbors to use by default for :meth:`kneighbors` queries.
+
+    weights : {'uniform', 'distance'} or None, default='uniform'
+        Weight function used in prediction.  Possible values:
+
+        - 'uniform' : uniform weights.  All points in each neighborhood
+          are weighted equally.
+        - 'distance' : weight points by the inverse of their distance.
+          in this case, closer neighbors of a query point will have a
+          greater influence than neighbors which are further away.
+
+    p : int, default=2
+        Power parameter for the Minkowski metric. When p = 1, this is
+        equivalent to using manhattan_distance (l1), and euclidean_distance
+        (l2) for p = 2. For arbitrary p, minkowski_distance (l_p) is used.
+
+    metric : str, default='minkowski'
+        Metric to use for distance computation. Default is "minkowski", which
+        results in the standard Euclidean distance when p = 2.
+
+    n_jobs : int, default=1
+        The number of parallel jobs to run for neighbors search.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+        Doesn't affect :meth:`fit` method.
+
+    verbose : bool, default=True
+        Whether to print progress messages or not.
+    
+        
+    Methods
+    -------
+    fit(X_train, y_train)
+        Fit the model using X_train as training data and y_train as target values.
+
+    predict(X_test)
+        Predict the class labels for the provided data.
+
+    save(path)
+        Save the model to the given path.
+
+    Examples
+    --------
+    >>> from src.Models.neighbors import KNN
+    >>> from sklearn.datasets import load_iris
+    >>> from sklearn.model_selection import train_test_split
+    >>> from sklearn.metrics import accuracy_score
+    >>> import pandas as pd
+    >>> import numpy as np
+    >>>
+    >>> iris = load_iris()
+    >>> X = pd.DataFrame(iris.data, columns=iris.feature_names)
+    >>> y = pd.Series(iris.target)
+    >>>
+    >>> X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    >>>
+    >>> model = KNN(k=5)
+    >>> model.fit(X_train, y_train)
+    >>> predictions = model.predict(X_test)
+    >>>
+    >>> print(f"Accuracy: {accuracy_score(y_test, predictions)}")
     '''
-    def __init__(self, k=3, n_jobs=1, metric='minkowski', p=2, verbose=True):
+    def __init__(self, k=3, n_jobs=1, metric='minkowski', p=2, weights='uniform', verbose=True):
         # Check for valid inputs
         if k < 1:
             raise ValueError("Invalid k. k must be greater than 0.")
@@ -32,6 +98,12 @@ class KNN:
         if not isinstance(p, int) and not isinstance(p, float):
             raise ValueError("Invalid p. p must be a number.")
         
+        if weights not in ['uniform', 'distance']:
+            raise ValueError("Invalid weights. Valid values are 'uniform' and 'distance'.")
+        
+        if not isinstance(weights, str):
+            raise ValueError("Invalid weights. weights must be a string.")
+        
         if n_jobs < 1 and n_jobs != -1:
             raise ValueError("Invalid n_jobs. n_jobs must be greater than 0. Use -1 to use all available cores.")
         
@@ -46,6 +118,7 @@ class KNN:
         self.k = k
         self.verbose = verbose
         self.metric = metric
+        self.weights = weights
 
         # Distance metric
         if self.metric == 'manhattan':
@@ -62,10 +135,20 @@ class KNN:
             self.n_jobs = n_jobs
 
     def get_nearest_neighbours(self, test):
-        # Get nearest neighbours using minkowski distance
+        # Get nearest neighbours using Minkowski distance
         distances = np.linalg.norm(self.X_train - test, ord=self.p, axis=1)
-        indices = np.argsort(distances)[:self.k]
-        return indices
+        
+        if self.weights == 'uniform':
+            indices = np.argsort(distances)[:self.k]
+        elif self.weights == 'distance':
+            indices = np.argsort(distances)[:self.k]
+            distances = distances[indices]
+            weights = 1 / distances
+            weights /= np.sum(weights)
+        else:
+            raise ValueError("Invalid weights. Valid values are 'uniform' and 'distance'.")
+        
+        return indices, weights
     
     def fit(self, X_train, y_train):
         # Save train data
@@ -81,9 +164,18 @@ class KNN:
         
     def _predict_instance(self, row):
         # Predict a single instance
-        neighbours = self.get_nearest_neighbours(row)
-        labels = [self.y_train.iloc[neighbour] for neighbour in neighbours]
-        return max(set(labels), key=labels.count)
+        indices, weights = self.get_nearest_neighbours(row)
+        labels = [self.y_train.iloc[neighbour] for neighbour in indices]
+        
+        if self.weights == 'uniform':
+            prediction = max(set(labels), key=labels.count)
+        elif self.weights == 'distance':
+            weighted_labels = [weights[i] * labels[i] for i in range(len(labels))]
+            prediction = max(set(weighted_labels), key=weighted_labels.count)
+        else:
+            raise ValueError("Invalid weights. Valid values are 'uniform' and 'distance'.")
+        
+        return prediction
 
     def predict(self, X_test):
         if self.verbose:
