fe01ec38611365bc2ae9954d0a4d3b58762a121f
Alifioditya <alifioditya@gmail.com>
2023-11-29 13:04:19 +0700

    feat: distance metric and p parameter for knn classifier

--- src/Models/neighbors.py
+++ src/Models/neighbors.py
@@ -12,10 +12,48 @@ class KNN:
     It is a lazy learning algorithm that stores all instances corresponding to training data in n-dimensional space.
     When an unknown discrete data is received, it analyzes the closest k number of instances saved (nearest neighbors) and returns the most common class as the prediction and for real-valued data it returns the mean of k nearest neighbors.
     '''
-    def __init__(self, k=3, n_jobs=1, verbose=True):
+    def __init__(self, k=3, n_jobs=1, metric='minkowski', p=2, verbose=True):
+        # Check for valid inputs
+        if k < 1:
+            raise ValueError("Invalid k. k must be greater than 0.")
+        
+        if not isinstance(k, int):
+            raise ValueError("Invalid k. k must be an integer.")
+        
+        if metric not in ['manhattan', 'euclidean', 'minkowski']:
+            raise ValueError("Invalid metric. Valid metrics are 'manhattan', 'euclidean' and 'minkowski'.")
+        
+        if not isinstance(metric, str):
+            raise ValueError("Invalid metric. metric must be a string.")
+
+        if p < 1:
+            raise ValueError("Invalid p. p must be greater than 0.")
+        
+        if not isinstance(p, int) and not isinstance(p, float):
+            raise ValueError("Invalid p. p must be a number.")
+        
+        if n_jobs < 1 and n_jobs != -1:
+            raise ValueError("Invalid n_jobs. n_jobs must be greater than 0. Use -1 to use all available cores.")
+        
+        if not isinstance(n_jobs, int):
+            raise ValueError("Invalid n_jobs. n_jobs must be an integer.")
+        
+        if not isinstance(verbose, bool):
+            raise ValueError("Invalid verbose. verbose must be a boolean.")
+    
+
         # Constructor
         self.k = k
         self.verbose = verbose
+        self.metric = metric
+
+        # Distance metric
+        if self.metric == 'manhattan':
+            self.p = 1
+        elif self.metric == 'euclidean':
+            self.p = 2
+        else:
+            self.p = p
 
         # If n_jobs is -1, use all available cores
         if n_jobs == -1:
@@ -24,8 +62,8 @@ class KNN:
             self.n_jobs = n_jobs
 
     def get_nearest_neighbours(self, test):
-        # Get nearest neighbours
-        distances = np.sqrt(np.sum((self.X_train - test) ** 2, axis=1))
+        # Get nearest neighbours using minkowski distance
+        distances = np.linalg.norm(self.X_train - test, ord=self.p, axis=1)
         indices = np.argsort(distances)[:self.k]
         return indices
     
